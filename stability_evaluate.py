23c7e1032c2b988b7bb552c9ab201648de6d80f0
wxz2002 <2046449167@sjtu.edu.cn>
2024-09-07 16:31:39 +0800

    fix bugs

--- stability_evaluate.py
+++ stability_evaluate.py
@@ -1,10 +1,23 @@
 import json
 
-def get_accs(results):
+def get_rome_accs(results):
     accs = []
     for result in results:
         after_edit_a_to_c_neurons = result['after_edit_a_to_c_neurons']
         stability_a_to_c_neurons = result['stability_a_to_c_neurons']
+        share_neurons = []
+        for neuron in after_edit_a_to_c_neurons:
+            if neuron in stability_a_to_c_neurons:
+                share_neurons.append(neuron)
+        acc = len(share_neurons) / len(after_edit_a_to_c_neurons)
+        accs.append(acc)
+    return accs
+
+def get_ft_accs(results):
+    accs = []
+    for result in results:
+        after_edit_a_to_c_neurons = result['a_to_c']
+        stability_a_to_c_neurons = result['stability_a_to_c']
         share_neurons = []
         for neuron in after_edit_a_to_c_neurons:
             if neuron in stability_a_to_c_neurons:
@@ -27,16 +40,16 @@ if __name__ == '__main__':
         lines = f.readlines()
         ft_multimodal_results = [json.loads(line) for line in lines]
     
-    rome_stability_original_answer_accs = get_accs(rome_stability_original_answer_results)
+    rome_stability_original_answer_accs = get_rome_accs(rome_stability_original_answer_results)
     average_rome_stability_original_answer_acc = sum(rome_stability_original_answer_accs) / len(rome_stability_original_answer_accs)
     
-    rome_stability_no_answer_accs = get_accs(rome_stability_no_answer_results)
+    rome_stability_no_answer_accs = get_rome_accs(rome_stability_no_answer_results)
     average_rome_stability_no_answer_acc = sum(rome_stability_no_answer_accs) / len(rome_stability_no_answer_accs)
     
-    ft_unimodal_accs = get_accs(ft_unimodal_results)
+    ft_unimodal_accs = get_ft_accs(ft_unimodal_results)
     average_ft_unimodal_acc = sum(ft_unimodal_accs) / len(ft_unimodal_accs)
 
-    ft_multimodal_accs = get_accs(ft_multimodal_results)
+    ft_multimodal_accs = get_ft_accs(ft_multimodal_results)
     average_ft_multimodal_acc = sum(ft_multimodal_accs) / len(ft_multimodal_accs)
     
     print('rome_stability_original_answer_acc:', average_rome_stability_original_answer_acc)
@@ -44,12 +57,12 @@ if __name__ == '__main__':
     print('ft_stability_unimodal_acc:', average_ft_unimodal_acc)
     print('ft_stability_multimodal_acc:', average_ft_multimodal_acc)
 
-    if rome_stability_original_answer_acc > rome_stability_no_answer_acc:
+    if average_rome_stability_original_answer_acc > average_rome_stability_no_answer_acc:
         print('结论3: True')
     else:
         print('结论3: False')
     
-    if ft_stability_unimodal_acc < ft_stability_multimodal_acc:
+    if average_ft_unimodal_acc < average_ft_multimodal_acc:
         print('结论4: True')
     else :
         print('结论4: False')
